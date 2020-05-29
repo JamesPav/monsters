ed0c18704b5f36cf135ed34048d6b18404740e46
Jim Pav <jamesvlid@hotmail.com>
2020-05-29 03:44:08 +0300

    Updates

---
+++ precache-manifest.145772d57fe35d1be42dd30b054c27a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64704c1d7a82b1d5eac9e1e2c97e95b3",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c517eb797116345afdda",
+    "url": "/monsters/static/css/main.7c380ff5.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c517eb797116345afdda",
+    "url": "/monsters/static/js/main.fbe4b1ff.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
